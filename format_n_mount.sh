a94a9b0a8ab7a12526e32adf67f5f07c14db7def
Hmz-x <hamzamusic34@gmail.com>
2024-06-23 16:22:23 +0300

    added formating FS and mounting partitions

--- format_n_mount.sh
+++ format_n_mount.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-root_check() {
+root_check() 
+{
     if [[ $UID -ne 0 ]]; then
         echo "Run as root. Exiting." >&2
         exit 1
     fi
 }
 
-determine_largest_disk() {
+determine_largest_disk() 
+{
     largest_disk=$(lsblk -dno NAME,SIZE | grep -Eo '^[^ ]+' | while read disk; do
         size=$(lsblk -dno SIZE /dev/$disk)
         echo "$size /dev/$disk"
@@ -19,7 +21,8 @@ determine_largest_disk() {
     [[ "$ans" == "y" || "$ans" == "Y" ]] || { echo "Exiting and avoiding overwrite."; exit 1; }
 }
 
-determine_boot() {
+determine_boot() 
+{
     if [ -d /sys/firmware/efi ]; then
         boot_sys="UEFI"
     else
@@ -27,7 +30,8 @@ determine_boot() {
     fi
 }
 
-create_partition_table() {
+create_partition_table() 
+{
     if [ "$boot_sys" = "UEFI" ]; then
         partition_table="gpt"
     else
@@ -50,9 +54,12 @@ create_partition_table() {
 
     echo "Creating HOME partition..."
     parted -s "$largest_disk" mkpart primary ext4 49664MiB 100%
+    
+    echo "\n\n"
 }
 
-change_partition_types() {
+change_partition_types()
+{
     # For UEFI systems, EFI partition type is set during creation
     # For BIOS systems, the boot flag needs to be set and the type needs to be linux
     if [ "$boot_sys" = "BIOS" ]; then
@@ -63,9 +70,57 @@ change_partition_types() {
     parted -s "$largest_disk" name 1 'BOOT'
     parted -s "$largest_disk" name 3 'ROOT'
     parted -s "$largest_disk" name 4 'HOME'
+
+    echo "\n\n"
 }
 
-verify_partition_table() {
+create_fs()
+{
+    # /dev/sda partition format: /dev/sdaX
+    [ "$largest_disk" == "/dev/sda" ] && disk_partition="$largest_disk"
+    # /dev/nvme0n1 partition format: /dev/nvme0n1pX
+    [ "$largest_disk" == "/dev/nvme0n1" ] && disk_partition="${largest_disk}p"
+
+    if [ "$boot_sys" = "UEFI" ]; then
+      echo "Formatting BOOT partition as FAT32..."
+      mkfs.fat -F32 "${disk_partition}1"
+    else
+      echo "Formatting BOOT partition as ext4..."
+      mkfs.ext4 "${disk_partition}1"
+    fi
+
+    echo "Formatting SWAP partition..."
+    mkswap "${disk_partition}2" 
+
+    echo "Formatting ROOT partition as ext4..."
+    mkfs.ext4 "${disk_partition}3"
+
+    echo "Formatting HOME partition as ext4..."
+    mkfs.ext4 "${disk_partition}4"
+
+    echo "\n\n"
+}
+
+mount_partitions()
+{
+    echo "Mounting BOOT partition to /mnt/boot..."
+    mount --mkdir -L BOOT /mnt/boot
+
+    echo "Activating SWAP partition..."
+    swapon "${disk_partition}2"
+
+    echo "Mounting ROOT partition to /mnt..."
+    mount -L ROOT /mnt
+
+    echo "Mounting HOME partition to /mnt/home..."
+    mount --mkdir -L HOME /mnt/home
+
+    echo "\n\n"
+}
+
+
+verify_partition_table() 
+{
     echo "Displaying created structure of ${largest_disk}:"
     parted "$largest_disk" print
 }
@@ -84,6 +139,9 @@ create_partition_table
 
 # Change partition types
 change_partition_types
+
+# Create File System for each partition
+create_fs
 
 # Verify partition table
 verify_partition_table
