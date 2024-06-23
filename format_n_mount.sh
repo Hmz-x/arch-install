be64bb4d77a8a87900b5a8c6e87333bffead4970
Hmz-x <hamzamusic34@gmail.com>
2024-06-23 15:05:07 +0300

    added changing partition table types

--- format_n_mount.sh
+++ format_n_mount.sh
@@ -18,7 +18,7 @@ determine_largest_disk()
   echo "Warning: This script will erase all data on ${largest_disk}. Use with caution."
 
   read -p "Continue [y/n]: " ans
-  [[ "$ans" != "y" || "$ans" != "Y" ]] && echo "Exiting and avoiding overwrite." && exit 1
+  [[ "$ans" == "y" || "$ans" == "Y" ]] || { echo "Exiting and avoiding overwrite."; exit 1; }
 }
 
 determine_boot()
@@ -55,10 +55,49 @@ create_partition_table()
   # Create HOME partition
   echo "Creating HOME partition..."
   echo -e "n\n\n\n\n\nw\n" | fdisk "$largest_disk"
+}
 
-  # Verify the partition table
-  echo "Partition table after changes:"
-  sudo fdisk -l /dev/sda
+change_partition_types()
+{
+
+  # BOOT Partition....................................
+  # For UEFI systems, EFI partition type to be assigned
+  [ "$boot_sys" = "UEFI" ] && partition_type_num=1
+  # For BIOS systems, Linux partition type to be assigned
+  [ "$boot_sys" = "BIOS" ] && partition_type_num=83
+
+  # Change BOOT partition type: EFI (UEFI) or Linux (BIOS)
+  echo -e "t\n1\n${partition_type_num}w\n" | fdisk "$largest_disk"
+
+  # Set bootable flag on BOOT
+  echo -e "a\n1\nw\n" | fdisk "$largest_disk"
+
+  # Change SWAP partition type: SWAP
+  echo -e "t\n2\n82w\n" | fdisk "$largest_disk"
+
+  # ROOT Partition....................................
+  # For UEFI systems, Linux root (x86-64) partition type to be assigned
+  [ "$boot_sys" = "UEFI" ] && partition_type_num=23
+  # For BIOS systems, Linux partition type to be assigned
+  [ "$boot_sys" = "BIOS" ] && partition_type_num=83
+
+  # Change ROOT partition type: 
+  echo -e "t\n3\n${partition_type_num}w\n" | fdisk "$largest_disk"
+
+  # HOME Partition....................................
+  # For UEFI systems, Linux home partition type to be assigned
+  [ "$boot_sys" = "UEFI" ] && partition_type_num=42
+  # For BIOS systems, Linux partition type to be assigned
+  [ "$boot_sys" = "BIOS" ] && partition_type_num=83
+
+  # Change HOME partition type
+  echo -e "t\n4\n${partition_type_num}w\n" | fdisk "$largest_disk"
+}
+
+verify_partition_table()
+{
+  echo "Displaying created structure of ${largest_disk}:"
+  fdisk -l "$largest_disk"
 }
 
 # Check if script is being run as root
@@ -72,3 +111,9 @@ determine_boot
 
 # Create new GPT partition table and partitions
 create_partition_table
+
+# Change partition types
+change_partition_types
+
+# Verify partition table
+verify_partition_table
