bcf0f351f13e157cc9d0ddc578dc013a1c9f0055
Hmz-x <hamzamusic34@gmail.com>
2024-06-23 15:34:42 +0300

    replacing fdisk with parted

--- format_n_mount.sh
+++ format_n_mount.sh
@@ -1,107 +1,73 @@
 #!/bin/bash
 
-root_check()
-{
-	if (($UID!=0)); then
-		echo "Run as root. Exitting." 2>&1
-		exit 1
-	fi
+root_check() {
+    if [[ $UID -ne 0 ]]; then
+        echo "Run as root. Exiting." >&2
+        exit 1
+    fi
 }
 
-determine_largest_disk()
-{
-  largest_disk=$(lsblk -dno NAME,SIZE | grep -Eo '^[^ ]+' | while read disk; do
-      size=$(lsblk -dno SIZE /dev/$disk)
-      echo "$size /dev/$disk"
-  done | sort -hr | head -n 1 | awk '{print $2}')
+determine_largest_disk() {
+    largest_disk=$(lsblk -dno NAME,SIZE | grep -Eo '^[^ ]+' | while read disk; do
+        size=$(lsblk -dno SIZE /dev/$disk)
+        echo "$size /dev/$disk"
+    done | sort -hr | head -n 1 | awk '{print $2}')
 
-  echo "Warning: This script will erase all data on ${largest_disk}. Use with caution."
+    echo "Warning: This script will erase all data on ${largest_disk}. Use with caution."
 
-  read -p "Continue [y/n]: " ans
-  [[ "$ans" == "y" || "$ans" == "Y" ]] || { echo "Exiting and avoiding overwrite."; exit 1; }
+    read -p "Continue [y/n]: " ans
+    [[ "$ans" == "y" || "$ans" == "Y" ]] || { echo "Exiting and avoiding overwrite."; exit 1; }
 }
 
-determine_boot()
-{
-	if [ -d /sys/firmware/efi ]; then
-		boot_sys="UEFI"
-	else
-		boot_sys="BIOS"
-	fi
+determine_boot() {
+    if [ -d /sys/firmware/efi ]; then
+        boot_sys="UEFI"
+    else
+        boot_sys="BIOS"
+    fi
 }
 
-create_partition_table()
-{
-  # GPT Partition table to be created if boot system is UEFI
-  [ "$boot_sys" = "UEFI" ] && partition_table="GPT" && partition_table_symbol="g"
-  # MBR(DOS) Partition table to be created if boot system is BIOS
-  [ "$boot_sys" = "BIOS" ] && partition_table="MBR/DOS" partition_table_symbol="o"
+create_partition_table() {
+    if [ "$boot_sys" = "UEFI" ]; then
+        partition_table="gpt"
+    else
+        partition_table="msdos"
+    fi
 
-  echo "Creating new $partition_table partition table on $largest_disk"
-  echo -e "${partition_table_symbol}\nw\n" | fdisk "$largest_disk"
+    echo "Creating new $partition_table partition table on $largest_disk"
+    parted -s "$largest_disk" mklabel "$partition_table"
 
-  # Create BOOT partition
-  echo "Creating Boot partition..."
-  echo -e "n\n\n\n\n+512M\nw\n" | fdisk "$largest_disk"
+    # Create partitions
+    echo "Creating Boot partition..."
+    parted -s "$largest_disk" mkpart primary fat32 1MiB 513MiB
+    [ "$boot_sys" = "UEFI" ] && parted -s "$largest_disk" set 1 boot on
 
-  # Create SWAP partition
-  echo "Creating SWAP partition..."
-  echo -e "n\n\n\n\n+4G\nw\n" | fdisk "$largest_disk"
+    echo "Creating SWAP partition..."
+    parted -s "$largest_disk" mkpart primary linux-swap 513MiB 4617MiB
 
-  # Create ROOT partition
-  echo "Creating ROOT partition..."
-  echo -e "n\n\n\n\n+45G\nw\n" | fdisk "$largest_disk"
+    echo "Creating ROOT partition..."
+    parted -s "$largest_disk" mkpart primary ext4 4617MiB 49664MiB
 
-  # Create HOME partition
-  echo "Creating HOME partition..."
-  echo -e "n\n\n\n\n\ny\nw\n" | fdisk "$largest_disk"
-
-  partprobe "$largest_disk"
+    echo "Creating HOME partition..."
+    parted -s "$largest_disk" mkpart primary ext4 49664MiB 100%
 }
 
-change_partition_types()
-{
+change_partition_types() {
+    # For UEFI systems, EFI partition type is set during creation
+    # For BIOS systems, the boot flag needs to be set and the type needs to be linux
+    if [ "$boot_sys" = "BIOS" ]; then
+        parted -s "$largest_disk" set 1 boot on
+    fi
 
-  # BOOT Partition....................................
-  # For UEFI systems, EFI partition type to be assigned
-  [ "$boot_sys" = "UEFI" ] && partition_type_num=1
-  # For BIOS systems, Linux partition type to be assigned
-  [ "$boot_sys" = "BIOS" ] && partition_type_num=83
-
-  # Change BOOT partition type: EFI (UEFI) or Linux (BIOS)
-  echo -e "t\n1\n${partition_type_num}w\n" | fdisk "$largest_disk"
-
-  # Set bootable flag on BOOT
-  echo -e "a\n1\nw\n" | fdisk "$largest_disk"
-
-  # Change SWAP partition type: SWAP
-  echo -e "t\n2\n82w\n" | fdisk "$largest_disk"
-
-  # ROOT Partition....................................
-  # For UEFI systems, Linux root (x86-64) partition type to be assigned
-  [ "$boot_sys" = "UEFI" ] && partition_type_num=23
-  # For BIOS systems, Linux partition type to be assigned
-  [ "$boot_sys" = "BIOS" ] && partition_type_num=83
-
-  # Change ROOT partition type: 
-  echo -e "t\n3\n${partition_type_num}w\n" | fdisk "$largest_disk"
-
-  # HOME Partition....................................
-  # For UEFI systems, Linux home partition type to be assigned
-  [ "$boot_sys" = "UEFI" ] && partition_type_num=42
-  # For BIOS systems, Linux partition type to be assigned
-  [ "$boot_sys" = "BIOS" ] && partition_type_num=83
-
-  # Change HOME partition type
-  echo -e "t\n4\n${partition_type_num}w\n" | fdisk "$largest_disk"
-
-  partprobe "$largest_disk"
+    echo "Setting partition names..."
+    parted -s "$largest_disk" name 1 'BOOT'
+    parted -s "$largest_disk" name 3 'ROOT'
+    parted -s "$largest_disk" name 4 'HOME'
 }
 
-verify_partition_table()
-{
-  echo "Displaying created structure of ${largest_disk}:"
-  fdisk -l "$largest_disk"
+verify_partition_table() {
+    echo "Displaying created structure of ${largest_disk}:"
+    parted "$largest_disk" print
 }
 
 # Check if script is being run as root
