7bdc788d6c439f375cbd38dbb42d48cfaeeaa077
Hmz-x <hamzamusic34@gmail.com>
2025-01-26 23:32:53 +0300

    01_format home_part_size updt

--- 01_format_n_mount.sh
+++ 01_format_n_mount.sh
@@ -2,13 +2,13 @@
 
 confirm_in()
 {
-	input="$1"
-	read -rp "${input} - confirm input [Y/n]: " user_ans
+    input="$1"
+    read -rp "${input} - confirm input [Y/n]: " user_ans
 
-	if [ -n "$user_ans" ] && [ "$user_ans" != "y" ] && [ "$user_ans" != "Y" ]; then
-		echo "Input is not confirmed. Returning." 2>&1
-		return 1
-	fi
+    if [ -n "$user_ans" ] && [ "$user_ans" != "y" ] && [ "$user_ans" != "Y" ]; then
+        echo "Input is not confirmed. Returning." 2>&1
+        return 1
+    fi
 }
 
 root_check() 
@@ -39,8 +39,6 @@ determine_boot()
     else
         boot_sys="BIOS"
     fi
-
-    echo -e "\n\n"
 }
 
 create_partition_table() 
@@ -50,6 +48,10 @@ create_partition_table()
     else
         partition_table="msdos"
     fi
+
+    echo "Wiping $largest_disk..."
+    wipefs -a "$largest_disk"
+    dd if=/dev/zero of="$largest_disk" bs=1M count=10 status=progress
 
     echo "Creating new $partition_table partition table on $largest_disk"
     parted -s "$largest_disk" mklabel "$partition_table"
@@ -66,13 +68,12 @@ create_partition_table()
     echo "Creating ROOT partition (50 GiB)..."
     parted -s "$largest_disk" mkpart primary ext4 1025MiB 51225MiB
 
-    # Create SWAP partition
     echo "Creating SWAP partition (16 GiB)..."
     home_start_mib=$((51225 + 16384))  # Reserve 16 GiB for SWAP
     parted -s "$largest_disk" mkpart primary linux-swap 51225MiB "$home_start_mib"MiB
 
     # Calculate remaining free space
-    total_disk_size_mib=$(lsblk -dno SIZE "$largest_disk" | awk '{print int($1*1024)}')  # Convert GiB to MiB
+    total_disk_size_mib=$(lsblk -dno SIZE "$largest_disk" | awk '{print int($1 * 1024)}')  # Convert GiB to MiB
     free_space_mib=$((total_disk_size_mib - home_start_mib))
     free_space_gib=$((free_space_mib / 1024))
 
@@ -81,8 +82,8 @@ create_partition_table()
     # Ask user for HOME partition size
     read -rp "Enter size for HOME partition in GiB (max: ${free_space_gib} GiB): " home_size_gb
     if ! [[ "$home_size_gb" =~ ^[0-9]+$ ]] || [ "$home_size_gb" -gt "$free_space_gib" ]; then
-        echo "Invalid input. Defaulting HOME partition size to 256 GiB."
-        home_size_gb=256
+        echo "Invalid input. Defaulting HOME partition size to ${free_space_gib} GiB."
+        home_size_gb=$free_space_gib
     fi
 
     home_size_mib=$((home_size_gb * 1024))
@@ -101,38 +102,25 @@ change_partition_types()
         echo "Setting first partition as bootable..."
         parted -s "$largest_disk" set 1 boot on
     fi
-
-#   Setting partition names does not work on my BIOS system as of current..
-#   Error: loop disk labels do not support partition name.
-#    echo "Setting partition names..."
-#    parted -s "$largest_disk" name 1 'BOOT'
-#    parted -s "$largest_disk" name 2 'SWAP'
-#    parted -s "$largest_disk" name 3 'ROOT'
-#    parted -s "$largest_disk" name 4 'HOME'
-
-    echo -e "\n\n"
 }
 
 create_fs()
 {
-    # /dev/sda partition format: /dev/sdaX
     [ "$largest_disk" == "/dev/sda" ] && disk_partition="$largest_disk"
-    # /dev/nvme0n1 partition format: /dev/nvme0n1pX
     [ "$largest_disk" == "/dev/nvme0n1" ] && disk_partition="${largest_disk}p"
 
+    echo "Formatting BOOT partition..."
     if [ "$boot_sys" = "UEFI" ]; then
-      echo "Formatting BOOT partition as FAT32..."
-      mkfs.fat -F32 "${disk_partition}1"
+        mkfs.fat -F32 "${disk_partition}1"
     else
-      echo "Formatting BOOT partition as ext4..."
-      mkfs.ext4 "${disk_partition}1"
+        mkfs.ext4 "${disk_partition}1"
     fi
-
-    echo "Formatting SWAP partition..."
-    mkswap "${disk_partition}2" 
 
     echo "Formatting ROOT partition as ext4..."
     mkfs.ext4 "${disk_partition}3"
+
+    echo "Formatting SWAP partition..."
+    mkswap "${disk_partition}2"
 
     echo "Formatting HOME partition as ext4..."
     mkfs.ext4 "${disk_partition}4"
@@ -142,15 +130,15 @@ create_fs()
 
 mount_partitions()
 {
+    echo "Mounting ROOT partition to /mnt..."
+    mount "${disk_partition}3" /mnt/
+
     echo "Mounting BOOT partition to /mnt/boot..."
     [ ! -d /mnt/boot ] && mkdir /mnt/boot
     mount "${disk_partition}1" /mnt/boot
 
     echo "Activating SWAP partition..."
     swapon "${disk_partition}2"
-
-    echo "Mounting ROOT partition to /mnt..."
-    mount "${disk_partition}3" /mnt/
 
     echo "Mounting HOME partition to /mnt/home..."
     mount --mkdir "${disk_partition}4" /mnt/home
@@ -164,65 +152,10 @@ verify_partition_table()
     parted "$largest_disk" print
     echo -e "\n\n"
 
-    echo "Check for correct mount points:"
-    lsblk "$largest_disk"
-    echo -e "\n\n"
-
     echo "Displaying results for 'genfstab -U /mnt'"
-    echo "If boot partition does not show simply (re)mount ${disk_partition}1 to /mnt/boot"
     genfstab -U /mnt
-    [ ! -d /mnt/etc ] && mkdir /mnt/etc
+
     echo -e "\n\n"
-
-    #read -rp "Write current partition table to /mnt/etc/fstab [y/n]: "
-    #[[ "$ans" == "y" || "$ans" == "Y" ]] && genfstab -U /mnt > /mnt/etc/fstab
-
-    read -rp "Mount ${disk_partition}1 to /mnt/boot [y/n]:" ans
-    confirm_in "$ans" || return
-
-    [ ! -d /mnt/boot ] && mkdir /mnt/boot
-    mount "${disk_partition}1" /mnt/boot
-
-    echo -e "\n\nNew genfstab output:"
-    genfstab -U /mnt
-
-    read -rp "Write to /mnt/etc/fstab [y/n]: " ans
-    confirm_in "$ans" || return
-    genfstab -U /mnt > /mnt/etc/fstab
-}
-
-add_boot_to_fstab() {
-
-    # Return if user selects answers besides y or Y
-    read -rp "Add boot partition to /mnt/etc/fstab [y/n]: " ans
-    [[ "$ans" == "y" || "$ans" == "Y" ]] || return 0
-
-    # Ensure that the boot partition is mounted
-    if ! mount | grep -q "/mnt/boot"; then
-        echo "Boot partition is not mounted. Please mount it first."
-        return 1
-    fi
-
-    # Get the UUID of the boot partition
-    boot_uuid=$(blkid -s UUID -o value "${disk_partition}1")
-    
-    # Ensure the UUID was found
-    if [ -z "$boot_uuid" ]; then
-        echo "Could not find UUID for boot partition."
-        return 1
-    fi
-
-    # Check if the UUID is already in fstab
-    if grep -q "$boot_uuid" /mnt/etc/fstab; then
-        echo "Entry for the boot partition already exists in /mnt/etc/fstab."
-        return 0
-    fi
-
-    # Add the boot partition entry to /mnt/etc/fstab
-    echo "# ${disk_partition}1" >> /mnt/etc/fstab
-    echo "UUID=$boot_uuid  /boot  vfat  rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,utf8,errors=remount-ro  0 2" >> /mnt/etc/fstab
-    echo "Boot partition added to /mnt/etc/fstab:"
-    tail -n 2 /mnt/etc/fstab
 }
 
 # Check if script is being run as root
@@ -248,6 +181,3 @@ mount_partitions
 
 # Verify partition table
 verify_partition_table
-
-# Add boot partition to fstab (If user chooses so)
-#add_boot_to_fstab
